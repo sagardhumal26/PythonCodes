a0fe1a9176faf91f40b0947541153016e660680c
sagardhumal26 <sagardhumal26@gmail.com>
2018-12-28 19:34:44 +0530

    Update BinaryTree.py
    Added DFS pre/in/post order methods

--- BinaryTree.py
+++ BinaryTree.py
@@ -1,3 +1,4 @@
+from asyncio.queues import Queue
 class Node:
     def __init__(self,data):
         self.left=None
@@ -8,13 +9,17 @@ class Node:
         if self.data:
             if (data < self.data):
                 if self.left is None:
+                    print("Left is None")
                     self.left=Node(data)
                 else:
+                    print("Adding to Left")
                     self.left.insert(data)
             else:
                 if self.right is None:
+                    print("Right is None")
                     self.right=Node(data)
                 else:
+                    print("Adding to Right")
                     self.right.insert(data)
         else:
             self.data=data
@@ -36,7 +41,48 @@ class Node:
             self.left.printTree()
         print(self.data)
         if self.right:
-            self.right.printTree()        
+            self.right.printTree()
+        
+    def maxDepth(self):
+        if self is None:
+            return 0
+        else:
+            ldepth=rdepth=0
+            if self.left:
+                ldepth = self.left.maxDepth()
+            if self.right:
+                rdepth = self.right.maxDepth()
+            return ldepth+1 if ldepth > rdepth else rdepth+1
+    
+    def DFS_preOrder(self):
+        if self:
+            print(self.data,end="->")
+        if self.left:
+            self.left.DFS_preOrder()
+        if self.right:
+            self.right.DFS_preOrder()
+            
+    def DFS_inOrder(self):
+        if self.left:
+            self.left.DFS_inOrder()
+        if self:
+            print(self.data,end="->")
+        if self.right:
+            self.right.DFS_inOrder()
+            
+    def DFS_postOrder(self):
+        if self.left:
+            self.left.DFS_postOrder()
+        if self.right:
+            self.right.DFS_postOrder()
+        if self:
+            print(self.data,end="->")
+                
+    def BFS(self):
+        pass
+                
+    def deleteNode(self):
+        pass
 
 if __name__=='__main__':
     root=Node(10)
@@ -46,3 +92,11 @@ if __name__=='__main__':
     root.printTree()
     print(root.findval(7))
     print(root.findval(8))
+    print(root.maxDepth())
+    root.DFS_preOrder()
+    print()
+    root.DFS_inOrder()
+    print()
+    root.DFS_postOrder()
+    print()
+    root.BFS()
